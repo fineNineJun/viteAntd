65b1bb6cbe55b71c31ed602b20583fae78ed7f73
fineNineJun <jun_059@163.com>
2022-02-18 11:06:43 +0800

    vuex配置

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { setupStore } from './store' // 状态管理
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+setupStore(app) // 引入状态管理
+
+app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import * as path from 'path'
+const resolve = (dir: string) => path.join(__dirname, dir)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@/":resolve("src/*"),
+      comps: resolve("src/components"),
+      store: resolve("src/store"),
+    }
+  }
 })
